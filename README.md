67c216616953815d6f07d315f5fc783baa967594
David Byron <davbyron@icloud.com>
2019-05-07 09:57:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hidden Markov Model
+
+> First steps to building my own Hidden Markov Model for predicting the probability of sequences of characters/words/strings in the English language.
+
+This script was produced using Python 3.7.2 and was originally submitted as homework for LING 38600 'Computational Linguistics' taught by John Goldsmith at the University of Chicago, spring quarter 2019.
+
+This repository contains the following files:
+
+- `hmm_1.py`: This script constitutes the first steps toward building my HMM. It calculates the probability assigned to a string by a probabilistic Finite-State Automata. All details are provided within the comments of the code.
+- `sample_text_hmm_1.txt`: Text file containing the nonse words *babi#* and *dida#*. This file can (and was) used to test the code on a small-scale level.
+- `english_1000.txt`: Text file containing 1000 English tokens for a larger-scale test of the code. This file was provided by Professor Goldsmith.
---
+++ hmm_1.py
@@ -0,0 +1,139 @@
+import random, re
+from pprint import pprint
+from math import log
+
+class State():
+    name = ''
+
+    def __init__(self, letter_list):
+        self.letter_list = letter_list
+
+    def __repr__(self):
+        return self.name
+
+def probs(items):
+    random.shuffle(items)
+    item_probs = {}
+
+    sum = 1
+    i = 0
+    for item in items:
+        if i < len(items) - 1:
+            rand = random.uniform(0, sum)
+            item_probs[item] = rand
+            sum -= rand
+        else:
+            item_probs[item] = sum
+        i += 1
+
+    return item_probs
+
+def forward(states, pi, this_word):
+    alpha = {}
+    for s in states:
+        alpha[(s, 0)] = pi[int(s.name[-1])]
+    for t in range(1, len(this_word) + 1):
+        for to_state in states:
+            alpha[(to_state, t)] = 0
+            for from_state in states:
+                alpha[(to_state, t)] += alpha[(from_state, t - 1)] * from_state.letter_probs[this_word[t - 1]] * from_state.a
+    return alpha
+
+def backward(states, this_word):
+    beta = {}
+    last = len(this_word)
+    for s in states:
+        beta[(s, last)] = 1
+    for t in range(len(this_word) - 1, -1, -1):
+        for from_state in states:
+            beta[(from_state, t)] = 0
+            for to_state in states:
+                beta[(from_state, t)] += beta[(to_state, t + 1)] * from_state.letter_probs[this_word[t]] * from_state.a
+    return beta
+
+def get_plog(a):
+    return -log(a, 2)
+
+letters = []
+wds = []
+
+with open('english1000.txt') as f:
+    for line in f:
+        line = line[:-1] + '#'
+        line = line.lower()
+        wds.append(line)
+        for c in line:
+            if c not in letters and re.search('[a-z]|#|\'|\.', c):
+                letters.append(c)
+
+num_states = int(input('How many states would you like to use?: '))
+VerboseFlag = False
+
+states = []
+pis = list(range(num_states))
+pi_probs = probs(pis)
+
+for num in range(num_states):
+    states.append(State(letters))
+
+m = 0
+for state in states:
+    state.name = f'State {str(m)}'
+    state.a = round(1 / num_states, 4)
+    state.letter_probs = probs(state.letter_list)
+    m += 1
+
+alphas = {}
+betas = {}
+alpha_totals = {}
+beta_totals = {}
+plogs = {}
+
+for w in wds:
+    alphas[w] = forward(states, pi_probs, w)
+    betas[w] = backward(states, w)
+    alpha_totals[w] = 0
+    beta_totals[w] = 0
+
+for w in alphas:
+    for alph in alphas[w]:
+        s, t = alph
+        if t == len(alphas[w]) / 2 - 1: # This is a problem for odd number of states
+            alpha_totals[w] += alphas[w][alph]
+
+for w in betas:
+    for bet in betas[w]:
+        s, t = bet
+        if t == 0:
+            beta_totals[w] += pi_probs[int(s.name[-1])] * betas[w][bet]
+
+for alph_sum in alpha_totals:
+    plog = get_plog(alpha_totals[alph_sum])
+    plogs[alph_sum] = plog
+
+pprint(plogs)
+pprint(sum(plogs.values()))
+
+j = 0
+if VerboseFlag == True:
+    print('-' * 33, '\n')
+    print('-\t Initialization\t\t -')
+    print('-' * 33)
+
+    for state in states:
+        print(f'\nCreating State {j}')
+        print('Transitions')
+
+        for num in range(num_states):
+            print(f'\tTo state\t{num}\t{state.a}')
+
+        print('\nEmission probabilities')
+        for letter in state.letter_probs:
+            print(f'\tLetter\t{letter}\t{state.letter_probs[letter]}')
+
+        j += 1
+
+    print('\n', '-' * 33)
+    print('Pi:')
+    for pi in pi_probs:
+        print(f'State\t{pi}\t{pi_probs[pi]}')
